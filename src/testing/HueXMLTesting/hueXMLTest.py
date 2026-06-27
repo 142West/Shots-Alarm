4daf92d7233e95feec13b625add067def487482c
Alex Tannenbaum <alex@alextannenbaum.com>
2026-06-27 14:42:51 -0400

    delete old files

--- src/testing/HueXMLTesting/hueXMLTest.py
+++
@@ -1,7 +0,0 @@
-import json
-
-f = open("../../config/hue.json")
-hueLights = json.load(f)
-print(hueLights)
-print(hueLights["bridgeIp"])
-print(hueLights["lights"][0])
--- src/testing/remoteSocketClient.py
+++
@@ -1,9 +0,0 @@
-
-import socket
-
-HOST = 'shots-alarm'  # The server's hostname or IP address
-PORT = 8675        # The port used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.send(b'ACTIVATE')
